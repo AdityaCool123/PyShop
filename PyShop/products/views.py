508afb65de659afdefc0a99dc4bd01ce9d9f0419
Aditya Cool <99129106+AdityaCool123@users.noreply.github.com>
2022-12-27 13:08:59 +0530

    Add files

---
+++ PyShop/products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products':products})
+
+
+def new(request):
+    return HttpResponse('New Products')
+
+
+def preview(request):
+    return render(request, 'preview.html')
